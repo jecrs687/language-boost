75691312d9bcc491c64001fcdb123847307186f7
Emanuel Cascone <emanuelcascone@gmail.com>
2024-02-01 16:15:43 -0300

    feat: inicial commit

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Platform, StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import app from './app.json'
+import * as Sharing from 'expo-sharing';
+import * as Linking from 'expo-linking';
+import * as Haptics from 'expo-haptics';
 
 export default function App() {
+  const values = {
+    device: 'Mobile',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    os: Platform.OS,
+    version: Platform.Version,
+    devicePixelRatio: Dimensions.get('window').scale,
+    appVersion: app.expo.version
+  }
+  const query = Object.keys(values).map(key => `${key}=${values[key]}`).join('&');
+  const functions = {
+    sharing: (values) => {
+      Sharing.shareAsync(...values)
+    },
+    linking: (values) => {
+      Linking.openURL(...values)
+    },
+    vibration: (values) => {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle[values])
+    }
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{
+        uri: `http://localhost:3001/?${query}`
+      }}
+      webviewDebuggingEnabled={true}
+      allowFileAccess={true}
+      allowsAirPlayForMediaPlayback={true}
+      allowsFullscreenVideo={true}
+      allowFileAccessFromFileURLs={true}
+      scrollEnabled={false}
+      onMessage={(event) => {
+        try {
+          const data = JSON.parse(event.nativeEvent.data);
+          functions?.[data.name]?.(data.content)
+        } catch (e) {
+          console.log(e);
+        }
+      }}
+      cacheEnabled={true}
+      cacheMode='LOAD_DEFAULT'
+    />
   );
 }
 
